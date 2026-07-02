d14368e62145b160d178040802b68e41b914de94
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-07-02 19:36:21 +0530

    simplify input in route

--- backend/app/routes/chat.py
+++ backend/app/routes/chat.py
@@ -1,10 +1,18 @@
+from app.utils.message_formatter import message_formatter
 from fastapi import APIRouter
-from app.schemas.chat import chatRequest,chatResponse
-from app.services.openai import generate
+from fastapi.responses import StreamingResponse
+from app.schemas.chat import chatRequest
+from app.services.llm import generate
 
 router=APIRouter()
 
-@router.post("/chat",response_model=chatResponse)
-async def chat(request:chatRequest):
-    reply=await generate(request.message)
-    return chatResponse(reply=reply)
+@router.post("/chat")
+async def chat(request: chatRequest):
+   
+    formatted_messages=message_formatter(request.messages)
+    async def stream():
+        async for chunk in generate(formatted_messages):
+            yield chunk
+
+    return StreamingResponse(stream(), media_type="text/plain")
+    
