931b4deda97e5f79174f6f0af9d982be17724591
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-07-04 18:53:45 +0530

    hook to connect with backend

--- frontend/hooks/useChatAi.ts
+++ frontend/hooks/useChatAi.ts
@@ -1,9 +1,9 @@
 import { useChat } from "@ai-sdk/react";
-import { DefaultChatTransport } from "ai";
+import { TextStreamChatTransport } from "ai";
 
 export function useChatAi(){
     return useChat({
-        transport:new DefaultChatTransport({
+        transport:new TextStreamChatTransport({
             api:"http://localhost:8000/chat",
         })
     })
