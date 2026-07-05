a88a4186fe570176466d76bece646ff916a70b64
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-07-05 20:38:13 +0530

    tools add

--- backend/app/tools/registry/registry.py
+++ backend/app/tools/registry/registry.py
@@ -1,7 +1,9 @@
 from app.integrations.shopify.orders import get_order_status
+from app.integrations.shopify.products import get_product_info
 
 
 TOOL_FUNCTIONS={
-    "get_order_status":get_order_status
+    "get_order_status":get_order_status,
+    "get_product_info":get_product_info
 }
     
