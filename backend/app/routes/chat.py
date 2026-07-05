47858c5c8d9635c56e090a7c4ad954345a777e08
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-07-05 23:10:53 +0530

    authentication add

--- backend/app/routes/chat.py
+++ backend/app/routes/chat.py
@@ -1,3 +1,5 @@
+from app.dependencies.auth import get_client_id_from_api_key
+from fastapi import Depends
 from app.utils.message_formatter import message_formatter
 from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
@@ -7,11 +9,11 @@ from app.services.llm import generate
 router=APIRouter()
 
 @router.post("/chat")
-async def chat(request: chatRequest):
+async def chat(request: chatRequest,client_id:str=Depends(get_client_id_from_api_key)):
    
     formatted_messages=message_formatter(request.messages)
     async def stream():
-        async for chunk in generate(formatted_messages):
+        async for chunk in generate(formatted_messages,client_id=client_id):
             yield chunk
 
     return StreamingResponse(stream(), media_type="text/plain")
