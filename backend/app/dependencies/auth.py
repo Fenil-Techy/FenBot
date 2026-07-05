2190b5b52578dd42d00314e9cf8dcdf58342b978
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-07-05 23:10:37 +0530

    refacator auth

--- backend/app/dependencies/auth.py
+++ backend/app/dependencies/auth.py
@@ -1,10 +1,11 @@
 
+from fastapi import Header
 from fastapi import HTTPException
 from app.dependencies.database_conn import get_pool
 from app.utils.apikeys import hash_key_check
 
-async def get_client_id_from_api_key(api_key:str,store_domain:str):
-    hash_key=hash_key_check(api_key)
+async def get_client_id_from_api_key(x_api_key:str=Header(...))->str:
+    hash_key=hash_key_check(x_api_key)
     pool=await get_pool()
     async with pool.acquire() as conn:
         row = await conn.fetchrow(
