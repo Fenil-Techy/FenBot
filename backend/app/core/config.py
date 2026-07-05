5dec8de64c7df521b7888abf47187fe2159ab788
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-07-05 20:33:21 +0530

    load whatsapp credentials

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,11 +3,17 @@ from pydantic_settings import BaseSettings,SettingsConfigDict
 class Settings(BaseSettings):
     OPENAI_API_KEY:str
     DATABASE_URL:str
+
     SHOPIFY_STORE_DOMAIN: str
     SHOPIFY_ACCESS_TOKEN: str
+
+    WHATSAPP_PHONE_NUMBER_ID: str
+    WHATSAPP_ACCESS_TOKEN: str
+    WHATSAPP_VERIFY_TOKEN: str
     
     model_config=SettingsConfigDict(
         env_file=".env",
-        env_file_encoding="utf-8"
+        env_file_encoding="utf-8",
+        extra="ignore"
     )
 settings=Settings()
