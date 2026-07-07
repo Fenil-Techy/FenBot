a5ddd27c30510223c26bd0d4fe21530c15262f93
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-07-07 14:01:23 +0530

    using dynamic apikey instead of hardcoded

--- frontend/hooks/useChatAi.ts
+++ frontend/hooks/useChatAi.ts
@@ -1,13 +1,13 @@
 import { useChat } from "@ai-sdk/react";
 import { TextStreamChatTransport } from "ai";
 
-export function useChatAi(){
-    return useChat({
-        transport:new TextStreamChatTransport({
-            api:"http://localhost:8000/chat",
-            headers:{
-                "X-Api-Key":process.env.NEXT_PUBLIC_FENBOT_API_KEY!
-            }
-        })
+export function useChatAi(apiKey: string) {
+  return useChat({
+    transport: new TextStreamChatTransport({
+      api: "http://localhost:8000/chat",
+      headers: {
+        "X-Api-Key": apiKey,
+      }
     })
+  });
 }
