a662e1e92d4a09febddfd5aff89a66885f88e2f1
Fenil Kapopara <fenilkapopara34@gmail.com>
2026-07-12 16:50:34 +0530

    visitor id creation and passing to backend

--- frontend/hooks/useChatAi.ts
+++ frontend/hooks/useChatAi.ts
@@ -1,10 +1,25 @@
 import { useChat } from "@ai-sdk/react";
 import { TextStreamChatTransport } from "ai";
 
+function getVisitorId(){
+  if (typeof window === "undefined") return
+
+  let id=localStorage.getItem("fenbot_visitor_id")
+  if (!id){
+    id=crypto.randomUUID()
+    localStorage.setItem("fenbot_visitor_id",id)
+  }
+  return id
+}
+
+
 export function useChatAi(chatbot_id: string) {
   return useChat({
     transport: new TextStreamChatTransport({
-      api: `http://localhost:8000/chat/${chatbot_id}`
+      api: `http://localhost:8000/chat/${chatbot_id}`,
+      body:{
+        visitor_id:getVisitorId()
+      }
     })
   });
 }
